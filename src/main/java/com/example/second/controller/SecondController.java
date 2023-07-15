06ca7e2d4a4dd30f73199f45f521759be3489db6
ef vl <efvl2022@gmail.com>
2023-07-15 22:56:21 +0200

    add s123 endpoint

---
+++ src/main/java/com/example/second/controller/SecondController.java
@@ -0,0 +1,21 @@
+package com.example.second.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/second")
+public class SecondController {
+
+    @GetMapping("/word")
+    public String getWord(){
+        return "second word";
+    }
+
+    @GetMapping("/s123")
+    public String getSentence(){
+        return "sentence 123";
+    }
+
+}
