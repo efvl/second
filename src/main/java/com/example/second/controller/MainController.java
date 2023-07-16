b99756d06a6035762d332cdfef4ecd21a7fb362e
ef vl <efvl2022@gmail.com>
2023-07-16 10:44:28 +0200

    add main controller

---
+++ src/main/java/com/example/second/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.example.second.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/main")
+public class MainController {
+
+    @RequestMapping("/one")
+    public String getOnePoint(){
+        return "main one";
+    }
+
+}
